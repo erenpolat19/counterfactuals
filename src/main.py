1223491ddacbb2bfe0d2b180a0f16ea329f37098
Eren Polat <eren@ucsb.edu>
2024-09-12 16:31:20 -0700

    kickstart

---
+++ src/main.py
@@ -0,0 +1,73 @@
+import argparse
+import sys
+
+
+sys.path.append('../')
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--batch_size', type=int, default=500, metavar='N',
+                    help='input batch size for training (default: 500)')
+parser.add_argument('--epochs', type=int, default=2000, metavar='N',
+                    help='number of epochs to train (default: 2000)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+
+parser.add_argument('--trn_rate', type=float, default=0.6, help='training data ratio')
+parser.add_argument('--tst_rate', type=float, default=0.2, help='test data ratio')
+
+parser.add_argument('--dim_z', type=int, default=16, metavar='N', help='dimension of z')
+parser.add_argument('--dim_h', type=int, default=16, metavar='N', help='dimension of h')
+parser.add_argument('--dropout', type=float, default=0.1)
+
+parser.add_argument('--dataset', default='imdb_m', help='dataset to use',
+                    choices=['community', 'ogbg_molhiv', 'imdb_m'])
+parser.add_argument('--lr', type=float, default=1e-3,
+                    help='learning rate for optimizer')
+parser.add_argument('--weight_decay', type=float, default=1e-5,
+                    help='weight decay')
+
+parser.add_argument('--experiment_type', default='train', choices=['train', 'test', 'baseline'],
+                    help='train: train CLEAR model; test: load CLEAR from file; baseline: run a baseline')
+
+args = parser.parse_args()
+
+
+
+'''
+    1. dataloader for mutag
+    2. pretrain the graph classifier
+    3. initialize factual and counterfactual models
+         (use the same encoder network w 2 decoders)
+    4. for each batch:
+        forward pass, encode graph G to z_mu, z_logvar
+        sample z with reparam. trick
+        decode explanations a_f, a_cf
+        encode a_cf to z_cf_mu, z_cf_logvar
+        sample z_cf
+        decode a_cf_f
+        we have a_f, a_cf, a_cf_f
+
+        loss = d(a_f, a_cf_f), I(y=1, a_cf_f), I(y=0, a_f), I(y=1, a_cf)??,
+            KL(z_u_logvar - prior_z_logvar), KL(a, a_cf), 
+
+        check proxy graph paper potentially for k_1 iterations on one loss etc., and k_2 on the other
+
+        check clear for setting some loss terms to 0 in the beginning e_0 epochs
+
+        backpropagate
+
+            -add something about the validation set? test our metrics on data_val
+            -possibly add perturbation stuff later?
+            -how to make sure of the causal graph relevant w counterfactual
+            -maybe construct a dataset or find a simple observable one (smth like shapes)
+            -look at how to make things global, anchors/prototypes? rkhs (ugh)?, 
+                                        unsup clustering a start? extract global for locals?
+            
+
+
+
+        
+
+
+
+'''
