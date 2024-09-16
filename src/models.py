a7842102bcda30632ce4c028614982110c55a802
Eren Polat <eren@ucsb.edu>
2024-09-16 14:41:54 -0700

    ,

--- src/models.py
+++ src/models.py
@@ -4,17 +4,13 @@ from torch.optim import Adam
 from torch_geometric.nn import GCNConv,global_max_pool, global_mean_pool
 import torch.nn.functional as F
 
-class GCNEncoder(nn.Module):
+class GraphModel(nn.Module):
     def __init__(self, x_dim, h_dim):
-        super(GCNEncoder, self).__init__()
         self.conv1 = GCNConv(x_dim, 1024)
         self.conv2 = GCNConv(1024, 512)
         self.conv3 = GCNConv(512, h_dim)
-        self.mu = 
-        self.logvar = 
-    
-    def forward(self, inputs):
-        x, edge_index, edge_weight = inputs
+
+    def forward(self, x, edge_index, edge_weight):
     
         out1 = self.conv1(x, edge_index, edge_weight=edge_weight)
         out1 = torch.nn.functional.normalize(out1, p=2, dim=1)
@@ -29,92 +25,81 @@ class GCNEncoder(nn.Module):
         out3 = F.relu(out3)
 
         return out3  
-    
-class FactualDecoder(nn.Module):
-    def __init__(self, encoder):
+
+class GraphEncoder(nn.Module):
+    def __init__(self, x_dim, h_dim, z_dim, embedder=None):
+        super(GraphEncoder, self).__init__()
+        if embedder == None:
+            embedder = GraphModel(x_dim, h_dim)
         self.embedder = embedder
+        self.h_dim = h_dim
+        self.z_dim = z_dim
+        self.encoder_mean = nn.Sequential(nn.Linear(self.h_dim + 1, self.z_dim), nn.BatchNorm1d(self.z_dim), nn.ReLU())
+        self.encoder_var = nn.Sequential(nn.Linear(self.h_dim + 1, self.z_dim), nn.BatchNorm1d(self.z_dim), nn.ReLU(), nn.Sigmoid())
+
+    def graph_pooling(self, x, type='mean'):
+        if type == 'max':
+            out, _ = torch.max(x, dim=1, keepdim=False)
+        elif type == 'sum':
+            out = torch.sum(x, dim=1, keepdim=False)
+        elif type == 'mean':
+            out = torch.sum(x, dim=1, keepdim=False)
+        return out
+    
+    def forward(self, x, edge_index, edge_weight, y_target): 
+        graph_emb = self.graph_pooling(self.embedder(x, edge_index, edge_weight))
+        
+        z_mu = self.encoder_mean(torch.cat((graph_emb, y_target), dim=1))
+        z_logvar = self.encoder_var(torch.cat((graph_emb, y_target), dim=1))
+
+        return z_mu,z_logvar
+    
+class Decoder(nn.Module):
+    def __init__(self, z_dim, output_size):
     
         self.decoder = nn.Sequential(
-            nn.Linear(hidden_dim * 2, hidden_dim * 2),
+            nn.Linear(z_dim * 2, z_dim * 2),
             nn.ReLU(),
-            nn.Linear(hidden_dim * 2, output_size)
+            nn.Linear(z_dim * 2, output_size)
         )
-class CounterfactualDecoder(nn.Module):
-    def encode(self, inputs):
-        graph_embedding = self.embedder(inputs)
-
-        mu = self.fc_mu(graph_embedding)
-        logvar = self.fc_logvar(graph_embedding)
-
-        return mu,logvar
-
-
-
-class GNN_MLP_VariationalAutoEncoder(nn.Module):
-    def __init__(self, feature_size, output_size):
-    super(GNN_MLP_VariationalAutoEncoder, self).__init__()
-    self.conv1 = GCNConv(feature_size, 1024)
-    self.conv2 = GCNConv(1024, 512)
-    self.conv3 = GCNConv(512, 256)
-    hidden_dim = 512
-    
-    self.decoder = nn.Sequential(
-        nn.Linear(hidden_dim * 2, hidden_dim * 2),
-        nn.ReLU(),
-        nn.Linear(hidden_dim * 2, output_size)
-    )
-
-    self.fc_mu = nn.Linear(256, hidden_dim)
-    self.fc_logvar = nn.Linear(256, hidden_dim)
-
-    def encode(self, inputs):
-        x, edge_index, edge_weight = inputs
-    
-        out1 = self.conv1(x, edge_index, edge_weight=edge_weight)
-        out1 = torch.nn.functional.normalize(out1, p=2, dim=1)
-        out1 = F.relu(out1)
-
-        out2 = self.conv2(out1, edge_index, edge_weight=edge_weight)
-        out2 = torch.nn.functional.normalize(out2, p=2, dim=1)
-        out2 = F.relu(out2)
-
-        out3 = self.conv3(out2, edge_index, edge_weight=edge_weight)
-        out3 = torch.nn.functional.normalize(out3, p=2, dim=1)
-        out3 = F.relu(out3)
-
-        input_lin = out3
-
-        mu = self.fc_mu(input_lin)
-        logvar = self.fc_logvar(input_lin)
-
-        return mu, logvar
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        scale = 1e-2
-        eps = torch.randn_like(std) * scale
-        return mu + eps * std
-    
-    def decode(self, z):
+    def forward(self, z):
         return torch.sigmoid(self.decoder(z))
 
-    def forward(self,inputs,beta, batch=None):
-        
-        mu, logvar = self.encode(inputs)
-        z = self.reparameterize(mu, beta * logvar)
-        
-        if batch is None:
-            out1, _ = torch.max(z, 0)
-            out1 = out1.unsqueeze(0)
-            out2 = torch.mean(z, 0).unsqueeze(0)
+class Explainer(nn.Module):
+    def __init__(self, encoder, z_dim, a_out_size, x_out_size):
+        self.encoder = encoder
+        self.decoder_a = Decoder(z_dim, a_out_size)
+        self.decoder_x = Decoder(z_dim, x_out_size)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
         else:
-            out1 = global_max_pool(z, batch)
-            out2 = global_mean_pool(z, batch)
+            return mu
+    def decode(self ,z_sample):
+        return self.decoder_a(z_sample), self.decoder_x(z_sample)
+    
+    def forward(self, x, edge_index, edge_weight, y_target, beta=1):
+        mu, logvar = self.encoder(x, edge_index, edge_weight, y_target)
+        z_sample = self.reparameterize(mu, beta * logvar)
 
-        reduce_z = torch.cat([out1, out2], dim=-1)
-        recon_x = self.decode(reduce_z)
+        recons_a, recons_x = self.decode(z_sample)
 
-        return recon_x, mu, logvar
+'''
+    g, y_g
+    y_cf = !y_g
+    encoder = Encoder(x_dim , h_dim, z_dim)
+
+    factExplainer = Explainer(encoder, z_dim, a_out_size, x_out_size)
+    cfExplainer = Explainer(encoder, z_dim, a_out_size, x_out_size)
+
+    factual_exp = factExplainer(x, edge_index, edge_weight, y_g)
+    cf_exp = cfExplainer(x, edge_index, edge_weight, y_cf)
+
+'''
+        
 
 
 class PROXYExplainer(BaseExplainer):
