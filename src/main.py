2b3a050728281bc31a795ab3dd444c4817dc23a1
Eren Polat <eren@ucsb.edu>
2024-09-30 11:15:50 -0700

    .

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 import argparse
 import sys
 import numpy as np
-from models import *
+import models
 import torch
-import torch.nn.functional as F
-
 # from pretrain_clf import * 
-import gcn
+import GCN
 
 def create_edge_embed(node_embeddings, edge_index):
     h_i = node_embeddings[edge_index[0]]  
@@ -14,7 +12,7 @@ def create_edge_embed(node_embeddings, edge_index):
 
     return torch.cat([h_i, h_j], dim=1)
 
-#gumbel-softmax reparam trick 
+# gumbel-softmax reparam trick 
 def sample_graph(self, sampling_weights, temperature=1.0, bias=0.0, training=True):
     if training:
         bias = bias + 0.0001  #apparently if bias is 0 there can be problems
@@ -52,6 +50,7 @@ parser.add_argument('--weight_decay', type=float, default=1e-5,
 
 parser.add_argument('--experiment_type', default='train', choices=['train', 'test', 'baseline'],
                     help='train: train CLEAR model; test: load CLEAR from file; baseline: run a baseline')
+parser.add_argument('--reg_coefs', default=(0.05, 1.0), help='reg coefs')
 parser.add_argument
 args = parser.parse_args()
 
@@ -59,89 +58,90 @@ args = parser.parse_args()
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 
-def train(decoder, explainer, optimizer_f, optimizer_cf train_loader, device):
-    # edge embeddings go here -- not sure where this code goes, messed up when copy pasting --whoops 
-    # edge embeddings = z, z_shape = (num_nodes, 20)
+def loss_f(pred, target, mask, reg_coefs):
+    
+    scale = 0.99
+    mask = mask*(2*scale-1.0)+(1.0-scale)
+    
+    cce_loss = F.cross_entropy(pred, target)
+    size_loss = torch.sum(mask) * reg_coefs[0]
+    mask_ent_reg = -mask * torch.log(mask) - (1 - mask) * torch.log(1 - mask)
+    mask_ent_loss = reg_coefs[1] * torch.mean(mask_ent_reg)
+    
+    return cce_loss + size_loss + mask_ent_loss
 
-    """ (LATEER IF WE DO THIS)
-    embedder = ...
-    explainer = Explainer(embedder, z_dim, a_out_size, x_out_size)
-    recons_a, recons_x, z_mu, z_logvar = explainer(x, edge_index, edge_weight, y_target)
-    """
-
+def train(clf_model, factual_explainer, optimizer_f, train_loader, device, args):
     for epoch in range(args.epochs):
-        decoder.train()
-        explainer.train()
+        factual_explainer.train()
 
         total_loss_f = 0
-        total_loss_cf = 0
 
         for batch in train_loader:
             x, edge_index, edge_weight, y_target = batch.x.to(device), batch.edge_index.to(device), batch.edge_weight.to(device), batch.y.to(device)
+            with torch.no_grad():
+                node_emb = clf_model.embedding(data.x, data.edge_index, data.edge_weights) # num_nodes x h_dim
+                edge_emb = create_edge_embed(data.x, data.edge_index) # E x 2*h_dim
+
+            expl_mask = factual_explainer(edge_emb)
+            sampling_weights = factual_explainer(input_expl)
+            mask = sample_graph(sampling_weights, t, bias=0.0).squeeze()
+
+            with torch.no_grad():
+                # Using the masked graph's edge weights
+                masked_pred = clf_model((data.x, data.edge_index, expl_mask), data.batch)  # Graph-level prediction
 
             optimizer_f.zero_grad()
-            optimizer_cf.zero_grad()
-
-            # Forward pass through Factual explainer
-            reconst = decoder(z)   
-
+  
             # Loss for factual explainer
-            loss_f = 
+            # loss_f = KL div + clf loss
+            reg_coefs = args.reg_coefs
+            loss_f = loss_f(masked_pred, y_target, mask, reg_coefs)
 
-            total_loss_f.backward()
+            loss_f.backward()
             optimizer_f.step()
 
-            # Forward pass through the CF explainer
-            recons_a, recons_x, z_mu, z_logvar = explainer(x, edge_index, edge_weight, y_target)
+            total_loss_f += loss_f.item()
 
-            # Loss for CF explainer
-            loss_cf = 
-
-            total_loss_cf.backward()
-            optimizer_cf.step()
-
-        print(f"Epoch {epoch + 1}/{args.epochs}, Factual Loss: {total_loss_f.item()}, CF Loss: {total_loss_cf.item()}")
-
-        # validate on validation set ?
+        print(f"Epoch {epoch + 1}/{args.epochs}, Factual Loss: {total_loss_f)}")
 
     print("Training complete!")
 
 
-def run(args, params):
+def run(args):
     dataset_name = args.dataset_name
+    device = "cpu"
+    """
+    load data for train, val, test
+    """
+    data = preprocess_ba_2motifs(dataset_name, padded=False)
+    train_loader, val_loader, test_loader = get_dataloaders(data, batch_size=64, val_split=0.1, test_split=0.1)
+
+    """
+    All we are doing now is: Batched graphs --> GCN to get this pretrained classifier's node embeddings
+    --> make these into edge embeddings, z_uv (E num edges x 2*h_dim) --> FactualExplainer MLP --> sample & get factual mask (E x 1) 
+    """
+    # params
     params['x_dim'] = 10
     params['num_classes'] = 2
-    clf_model = GCN(params['x_dim'], params['num_classes'])
+    # embedder
+    clf_model = GCN(params['x_dim'], params['num_classes']).to(device)              # load best model
+    checkpoint = torch.load('best_model.pth')                                           # load the model state dict
+    clf_model.load_state_dict(checkpoint['model_state_dict'])
+    clf_model.eval()                                                                # set the model to evaluation mode
 
-    checkpoint = torch.load('clf.pth')
-    clf_model.load_state_dict(checkpoint)
-
-    #eval mode
-    clf_model.eval()
-    
+    # Factual Explainer MLP
     expl_embedding = args.h_dim * 2
-    explainer_model = FactualExplainer(expl_embedding)
+    factual_explainer = FactualExplainer(expl_embedding)
+    optimizer_f = optim.Adam(explainer.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+    train(clf_model, factual_explainer, optimizer_f, train_loader, device, args)
     
-    train_loader = None
-    for data in train_loader:
-        with torch.no_grad():
-            node_emb = clf_model.embedding(data.x, data.edge_index, data.edge_weights) #num_nodes x h_dim
-            edge_emb = create_edge_embed(data.x, data.edge_index) #E x 2*h_dim
-
-        sampling_weights = explainer_model(edge_emb)
-        expl_mask = sample_graph(sampling_weights, t, bias=sample_bias).squeeze()
-
-        with torch.no_grad():
-            # Using the masked graph's edge weights
-            masked_pred = clf_model((data.x, data.edge_index, expl_mask), data.batch)  # Graph-level prediction
-
-        #loss with KL-div, and cross entropy between masked_pred, y_true for graphs
-    
-
-
-
-
 run(args)
+
+
+
+
+
 
 '''1. dataloader for mutag
         2. pretrain the graph classifier
